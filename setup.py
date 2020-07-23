0321b156018d6be135f9d7c1a360c24cb63aaf8a
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2020-07-23 12:48:12 +0200

    Uploading Files

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: TTS text to speech :: snLionel90 ",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
--- tts_PyQuendo.py
+++ tts_PyQuendo.py
@@ -3,6 +3,8 @@ import os
 import pyttsx3
 import speech_recognition as sr
 
+__author__ = "sn.lionel90"
+
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, QRect, QFileInfo
@@ -21,8 +23,7 @@ class Window (QTabWidget): #clase ventana principal
         self.setMinimumSize(440,350) #tamaño minimo
         self.setWindowTitle('TTS, Texto to Speech PyQuendo') #texto de cabecera (titulo)
         self.addTab(TextToVoiceTab(), ' Text to Voice') #pestaña 1
-        self.addTab(VoiceToTextTab(),'Voice to Text')
-        self.addTab(AboutTab(),'About')
+        self.addTab(VoiceToTextTab(), 'Voice To Text')
 
 class TextToVoiceTab(QWidget):
     def __init__(self):
@@ -50,7 +51,7 @@ class TextToVoiceTab(QWidget):
 
     def callTextovoz(self): #llamando a la funcion del evento del boton aceptar
         texto = self.textArea.toPlainText() # a texo plano 
-        funcionTextToVoice(texto) #llamada a la funcion de traduccion 
+        TextToVoice(texto) #llamada a la funcion de traduccion 
     
     def salir(self):
         exit(0)
@@ -58,36 +59,32 @@ class TextToVoiceTab(QWidget):
 class VoiceToTextTab(QWidget):
     def __init__(self):
         super().__init__() #llamada de la funcion anterior
-        boton_Abrir = QPushButton('Abrir fichero', self) #inicializacion del boton
-        boton_Abrir.setGeometry(QRect(60,260,85,27)) #posicion y tamaño del boton
-        boton_Abrir.clicked.connect(self.Abrir) #llamando a la funcion de salir
 
-        boton_salir = QPushButton('Salir', self) #inicializacion del boton
-        boton_salir.setGeometry(QRect(150,260,85,27)) #posicion y tamaño del boton
-        boton_salir.clicked.connect(self.salir) #llamando a la funcion de salir
+        boton_escucha = QPushButton('Ejecutar Tarea', self)
+        boton_escucha.setGeometry(QRect(320,35,85,27))
+        boton_escucha.clicked.connect(self.callVoiceTotext)
 
-        labelIntro = QLabel("Abrir audio para leer su contenido", self)
-        labelIntro.setGeometry(QRect(60,10, 301, 80))
-        labelIntro.setFont(FONT2)
-        labelIntro.setAlignment(Qt.AlignCenter)
+        self.chekBox = QCheckBox('Utilizar el microfono', self)
+        self.chekBox.setGeometry(QRect(10,35,300,20))
 
-        label = QLabel("PyQuendo ©Sn.Lionel90" ,self) #titulo de la pestaña
-        label.setGeometry(QRect(60,10, 301,31)) #tamaño y posicion de la pestalña
-        label.setFont(FONT) #obtengo la fuente de la variable
-        label.setAlignment(Qt.AlignCenter)
+        self.textArea = QTextEdit(self)
+        self.textArea.setGeometry(QRect(15,60,390,160))
+        self.textArea.setReadOnly(True)
 
-    def Abrir(self):
-       print ('ola qkhe ase')
+        lbl = QLabel("PyQuendo ©Sn.Lionel90", self)
+        lbl.setGeometry(QRect(60,10,301,31))
+        lbl.setFont(FONT2)
+        lbl.setAlignment(Qt.AlignCenter)
 
-    def salir(self):
-        exit(0)
+    def callVoiceTotext(self):
+        useMicro = self.chekBox.isChecked()
 
-class AboutTab(QWidget):
-    def __init__(self):
-        super().__init__()
+        if not useMicro:
+            text_box = None
 
+        self.textArea.setPlainText(voiceToText(text_box))
 
-def funcionTextToVoice(texto): #funcion de traduccion
+def TextToVoice(texto): #funcion de traduccion
     """"
     parametros: texto -->str
     Texto traduciodo a  voz
@@ -96,6 +93,24 @@ def funcionTextToVoice(texto): #funcion de traduccion
     engine.setProperty ('ratio' ,100)
     engine.say (texto) #que diga lo que pasemos por texto
     engine.runAndWait()
+
+def voiceToText(self):
+    """"
+    parametros: pathFile -->str
+    returns: audio convertido a string
+    """
+    MicroEnchufao = True
+    ñacañaca = ''
+    r = sr.Recognizer()
+    if MicroEnchufao:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+            try:
+                ñacañaca = r.recognize_google(audio)
+            except Exception:
+                ñacañaca = 'nose entienda na'
+    return f'has dicho: \n {ñacañaca}' 
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
